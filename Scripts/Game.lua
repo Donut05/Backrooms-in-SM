791db8da5ada7378979ab8e595e66449350512c2
Illumina <83984076+RedBoi-1@users.noreply.github.com>
2022-06-11 16:24:20 -0400

    Changed Survival Game To Creative Game temporarilily

--- Scripts/Game.lua
+++ Scripts/Game.lua
@@ -1,6 +1,6 @@
-dofile "$SURVIVAL_DATA/Scripts/game/SurvivalGame.lua"
+dofile "$GAME_DATA/Scripts/game/CreativeGame.lua"
 
-Game = class( SurvivalGame )
+Game = class( CreativeGame )
 
 function Game.server_onCreate( self )
     SurvivalGame.server_onCreate( self )
